a1550d982ee8f7a74e8ac21a59fcf95e4b9b8227
Jason Hinton <jason_hinton@att.net>
2020-07-09 01:00:09 -0400

    post

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 const server = express();
 
 server.use(express.json());
 
+let users = [];
 
 
-
-
-
+// Post
+server.post("api/users", (req, res) => {
+	const newUser = req.body;
+	newUser.id = shortid.generate();
+	try {
+		if (!newUser.name || !newUser.bio) {
+			res
+				.status(400)
+				.json({ errorMessage: "Please provide name and bio for the user" });
+		} else {
+			users.push(newUser);
+			res.status(201).json(newUser);
+		}
+	} catch {
+		res
+			.status(500)
+			.json({
+				errorMessage:
+					"There was an error while saving the user to the database",
+			});
+	}
+});
 
 const PORT = 5000;
 server.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}`);
-})
+	console.log(`listening on http://localhost:${PORT}`);
+});
