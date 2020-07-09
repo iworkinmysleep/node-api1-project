9c44ccd8544df8ad9b046179e62e2e21780bd93d
Jason Hinton <jason_hinton@att.net>
2020-07-08 22:40:08 -0400

    boiler plate

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const shortid = require('shortid');
+
+const server = express();
+
+server.use(express.json());
+
+
+
+
+
+
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+})
